3aebf537bd739a962f5648b6a9e60db6bea94ba2
Phenox124 <blyall44@gmail.com>
2014-09-02 16:34:55 -0600

    Reformatted the code/made some small changes

--- Secondary Computers.lua
+++ Secondary Computers.lua
@@ -1,43 +1,46 @@
 mon = peripheral.wrap("back")
  
 function touch()
-        event, a, b, c = os.pullEvent("monitor_touch")
+    event, a, b, c = os.pullEvent("monitor_touch")
  
-        if a == "back" then
+    if a == "back" then
         redstone.setOutput("right", true)
-                mon.clear()
-                mon.setBackgroundColor(colors.red)
-                mon.clear()
-                mon.setCursorPos(1, 3)
-                mon.write("Working")
-        end
-        event, a, b, c = os.pullEvent("monitor_touch")
+        mon.clear()
+        mon.setBackgroundColor(colors.red)
+        mon.clear()
+        mon.setCursorPos(1, 3)
+        mon.write("Working")
+    end
+
+    event, a, b, c = os.pullEvent("monitor_touch")
  
-        if a == "back" and redstone.getOutput("right") == true then
-                redstone.setOutput("right", false)
-                load()
-        end
+    if a == "back" and redstone.getOutput("right") == true then
+        redstone.setOutput("right", false)
+        load()
+    end
 end
  
 function active()
-        rednet.send(202, "I'm Online!")
-        load()
+    rednet.send(202, "I'm Online!")
+    load()
 end
  
 function load()
-        mon.setBackgroundColor(colors.lime)
-        mon.clear()
-        mon.setTextScale(1)
-        mon.setCursorPos(2, 3)
-        mon.write("Ready")
-        touch()
+    mon.setBackgroundColor(colors.lime)
+    mon.clear()
+    mon.setTextScale(1)
+    mon.setCursorPos(2, 3)
+    mon.write("Ready")
+    touch()
 end
  
 while true do
-  if rednet.isOpen() == false then
-                  rednet.open("left")
-                  active()
-         else
-                  active()
+    if rednet.isOpen() == false and redstone.getOutput("right") == true then
+    	rednet.open("left")
+    	redstone.setOutput("right", false)
+        active()
+    else
+    	redstone.setOutput("right", false)
+        active()
   end
 end
