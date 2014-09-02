40e4c231c08ec829692f91bfb4ba197a3f051873
RaemonDamon <raemon_damon@hotmail.com>
2014-09-02 04:34:11 -0700

    Working Code without Rednet

--- Secondary Computers.lua
+++ Secondary Computers.lua
@@ -1,55 +1,43 @@
 mon = peripheral.wrap("back")
-
-function touch2()
-	event, a, b, c = os.pullEvent("monitor_touch")
-
-	if a == "back" then
-		redstone.setOutput("top", false)
-		--os.sleep(3)
-		load()
-	end
-end
-
+ 
 function touch()
-	event, a, b, c = os.pullEvent("monitor_touch")
-
-	if a == "back" then
-		redstone.setOutput("top", true)
-		--os.sleep(3)
-		mon.clear()
-		mon.setBackgroundColor(colors.red)
-		mon.clear()
-		mon.setCursorPos(1, 3)
-		mon.write("Working")
-		--os.sleep(3)
-		touch2()
-	end
+        event, a, b, c = os.pullEvent("monitor_touch")
+ 
+        if a == "back" then
+        redstone.setOutput("right", true)
+                mon.clear()
+                mon.setBackgroundColor(colors.red)
+                mon.clear()
+                mon.setCursorPos(1, 3)
+                mon.write("Working")
+        end
+        event, a, b, c = os.pullEvent("monitor_touch")
+ 
+        if a == "back" and redstone.getInput("right") == true then
+                redstone.setOutput("right", false)
+                load()
+        end
 end
-
-function load()
-	--sleep(5)
-	mon.setBackgroundColor(colors.lime)
-	mon.clear()
-	mon.setTextScale(1)
-	mon.setCursorPos(2, 3)
-	mon.write("Ready")
-	touch()
-end
-
+ 
 function active()
-	rednet.send(202, "I'm Online!")
-	load()
+        rednet.send(202, "I'm Online!")
+        load()
 end
-
+ 
+function load()
+        mon.setBackgroundColor(colors.lime)
+        mon.clear()
+        mon.setTextScale(1)
+        mon.setCursorPos(2, 3)
+        mon.write("Ready")
+        touch()
+end
+ 
 while true do
-	if rednet.isOpen() == false then
-			rednet.open("left")
-			active()
-		else
-			active()
-	end
+  if rednet.isOpen() == false then
+                  rednet.open("left")
+                  active()
+         else
+                  active()
+  end
 end
-
-
-
-
